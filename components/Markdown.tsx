5cd8af8730bb7623d68c3d126d0f07e645770b76
Matthew Caseres <matthewcaseres@outlook.com>
2021-07-02 12:03:58 -0500

    enable math

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,19 +1,20 @@
 import ReactMarkdown from "react-markdown";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 export default function Markdown({ content }: { content: string }) {
   return (
     <div className="prose dark:prose-dark">
-
-    <ReactMarkdown
-      components={{
-        p: (props) => <p {...props} style={{ margin: 0 }} />,
-        pre: ({node}) => <div>
-          {JSON.stringify(node)}
-        </div>
-      }}
-    >
-      {content}
-    </ReactMarkdown>
+      <ReactMarkdown
+        components={{
+          p: (props) => <p {...props} style={{ margin: 0 }} />,
+          pre: ({ node }) => <div>{JSON.stringify(node)}</div>,
+        }}
+        remarkPlugins={[remarkMath]}
+        rehypePlugins={[rehypeKatex]}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
   );
 }
